52a9d826ed8d8f59ff42512df0cf1f58404978a5
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-02-04 23:57:24 +0530

    crud done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import { MdDeleteForever } from "react-icons/md";
+
 const App = () => {
 
   const [product, setproduct] = useState([])
@@ -7,8 +9,6 @@ const App = () => {
   function fetchData() {
     axios.get("http://localhost:3000/api/notes")
       .then((res) => {
-        console.log(res.data.notes);
-
         setproduct(res.data.notes);
       })
   }
@@ -22,7 +22,25 @@ const App = () => {
 
     const { product, price, amount, image } = e.target.elements
 
-    setproduct(product.value, price.value, image.value, amount.value)
+    const newProduct = {
+      product: product.value,
+      price: price.value,
+      image: image.value,
+      amount: amount.value,
+    }
+
+    axios.post("http://localhost:3000/api/notes", newProduct)
+      .then((res) => {
+        setproduct((prev) => [...prev, res.data.note])
+
+      })
+  }
+
+  function deleteHandle(pro_id) {
+    axios.delete("http://localhost:3000/api/notes/" + pro_id)
+      .then(() => {
+        fetchData()
+      })
   }
 
   return (
@@ -41,12 +59,14 @@ const App = () => {
           <label htmlFor="" className='text-2xl'>Price : </label>
           <input name='price' type="text" className='border-2 border-white ' style={{ padding: "0.5rem 3rem" }} />
         </div>
-        <button className='h-15 w-50 bg-teal-700 text-white/50 text-2xl rounded-2xl :active'>add item</button>
+        <div className='flex gap-10 '>
+          <button className='h-15 w-50 bg-teal-700 text-white/50 text-2xl rounded-2xl active:scale-90'>add item</button>
+        </div>
       </form>
       <div className='h-[68%] w-full overflow-y-auto' style={{ padding: "2rem" }}>
         <div className='notes flex flex-wrap h-full'>
           {product.map((elem, indx) => {
-            return <div className='h-100 w-80 bg-zinc-500/40 rounded-2xl ' style={{ marginRight: "50px" }} key={indx}>
+            return <div className='h-120 w-80 bg-zinc-500/40 rounded-2xl ' style={{ marginRight: "100px" }} key={indx}>
               <div className='h-full w-full flex flex-col items-center gap-10 text-white' style={{ padding: "1.5rem" }}>
                 <div className='h-50 w-70'>
                   <img src={elem.image} alt="" className='h-full w-full object-cover rounded-2xl' />
@@ -55,6 +75,13 @@ const App = () => {
                   <h1>Product : {elem.product}</h1>
                   <h1>product price : {elem.price}</h1>
                   <h1>quantity left : {elem.amount}</h1>
+                </div>
+                <div className='flex justify-between w-full' style={{ padding: "0rem 1rem" }}>
+                  <div className='h-12 w-12 bg-red-800 rounded-2xl flex items-center justify-center text-2xl cursor-pointer ' onClick={() => {
+                    deleteHandle(elem._id)
+                  }}>
+                    <MdDeleteForever />
+                  </div>
                 </div>
               </div>
             </div>
